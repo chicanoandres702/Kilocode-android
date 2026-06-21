600945e291217bd7c77991ea7039b8eda47adbfc
chicanoandres702 <chicanoandres702@users.noreply.github.com>
2026-06-21 03:12:16 +0000

    fix: add jitpack repository for compose-markdown, Resolves: #12

--- kilocode-android/settings.gradle.kts
+++ kilocode-android/settings.gradle.kts
@@ -2,15 +2,8 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
-        gradlePluginPortal()
-    }
-}
+        maven { url = uri("https://jitpack.io") }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
     }
 }
 
